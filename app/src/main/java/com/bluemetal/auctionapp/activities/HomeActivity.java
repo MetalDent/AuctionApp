a442135ffc28d8ad25c809277d7da089d106eefd
MetalDent <priyapandya274@gmail.com>
2019-02-04 23:26:15 +0530

    Added Login and Sign Up activities

---
+++ app/src/main/java/com/bluemetal/auctionapp/activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.bluemetal.auctionapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bluemetal.auctionapp.activites.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/bluemetal/auctionapp/activities/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.bluemetal.auctionapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bluemetal.auctionapp.activites.R;
+
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
