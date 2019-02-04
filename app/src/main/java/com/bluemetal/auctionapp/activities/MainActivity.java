6bae72b294c2ff5a541d3c3dedc54ef5bce711a7
Swaraj1998 <swarajhota353@gmail.com>
2019-02-04 22:51:01 +0530

    Basic Setup

--- app/src/main/java/com/bluemetal/auctionapp/activities/MainActivity.java
+++ app/src/main/java/com/bluemetal/auctionapp/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.bluemetal.auctionapp;
+package com.bluemetal.auctionapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.bluemetal.auctionapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
