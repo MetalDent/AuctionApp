e200eea87e9c1e49a2cd5b8d4511efd369f15c56
MetalDent <priyapandya274@gmail.com>
2019-02-06 00:21:55 +0530

    Added links (not working properly)

--- app/src/main/java/com/bluemetal/auctionapp/activities/HomeActivity.java
+++ app/src/main/java/com/bluemetal/auctionapp/activities/HomeActivity.java
@@ -1,20 +1,23 @@
 package com.bluemetal.auctionapp.activities;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.bluemetal.auctionapp.R;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,37 +32,45 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if(mToggle.onOptionsItemSelected(item)) {
-            if (id == R.id.db) {
-
-                Intent DashboardActivity = new Intent(HomeActivity.this, DashboardActivity.class);
-                startActivity(DashboardActivity);
-
-            } else if (id == R.id.pro) {
-
-                Intent ProfileActivity = new Intent(HomeActivity.this, ProfileActivity.class);
-                startActivity(ProfileActivity);
-
-            } else if (id == R.id.add) {
-
-                Intent MyProductsActivity = new Intent(HomeActivity.this, MyProductsActivity.class);
-                startActivity(MyProductsActivity);
-
-            } else if (id == R.id.logout) {
-
-                FirebaseAuth.getInstance().signOut();
-                Intent loginActivity = new Intent(HomeActivity.this, LogInActivity.class);
-                startActivity(loginActivity);
-                finish();
-
-            }
+        if (mToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.db) {
+
+            Intent DashboardActivity = new Intent(HomeActivity.this, DashboardActivity.class);
+            startActivity(DashboardActivity);
+
+        } else if (id == R.id.pro) {
+
+            Intent ProfileActivity = new Intent(HomeActivity.this, ProfileActivity.class);
+            startActivity(ProfileActivity);
+
+        } else if (id == R.id.add) {
+
+             Intent MyProductsActivity = new Intent(HomeActivity.this, MyProductsActivity.class);
+             startActivity(MyProductsActivity);
+
+        } else if (id == R.id.logout) {
+
+            FirebaseAuth.getInstance().signOut();
+            Intent loginActivity = new Intent(HomeActivity.this, LogInActivity.class);
+            startActivity(loginActivity);
+            finish();
+
+        } else {
+
+            Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+        }
         DrawerLayout drawer = findViewById(R.id.drawer);
         drawer.closeDrawer(GravityCompat.START);
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     @Override
